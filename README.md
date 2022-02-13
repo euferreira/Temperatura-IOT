eecfe3969f22fe6a0f25a1830005951b8c7a5910
Fernando Ferreira <fernandferreira@hotmail.com>
2022-02-13 12:41:51 -0300

    feat: Implementação do esquema

--- README.md
+++
@@ -1,2 +0,0 @@
-# Temperatura-IOT
- Projeto de integração com Arduino + Leitor de temperatura DHT11 + shield ethernet
--- temperatura-api/controllers/TemperaturaController.js
+++ temperatura-api/controllers/TemperaturaController.js
@@ -15,7 +15,7 @@ class TemperaturaController {
     static async getAndCount(req, res) {
         try {
             const all = await temperaturaService.findAndCountAll();
-            return all.length > 0 ? res.status(200).json(all) : res.status(204);
+            return res.status(200).json(all);
         }
         catch (e) {
             return res.status(500).json({send: e.message});
@@ -24,6 +24,7 @@ class TemperaturaController {
 
     static async insert(req, res) {
         try {
+            console.log(req.body);
             const temp = await temperaturaService.insert(req.body);
             return temp !== null ? res.status(201).json(temp) : res.status(400).json({send: "Erro na inserção!"});
         }
